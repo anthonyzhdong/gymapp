fec392a62cca2a291c0a0bc2bccebdbe0a293c05
donan928 <donan928@student.otago.ac.nz>
2025-03-12 14:50:57 +1300

    protectedroute to check token

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -9,14 +9,40 @@ function ProtectedRoute({children}){
 
 
     const refreshToken = async() => {
-
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try{
+            const response = await api.post("/auth/refresh/", {
+                refresh: refreshToken,
+            });
+            if(resizeBy.status === 200){
+                localStorage.setItem(ACCESS_TOKEN, response.data.access)
+                setIsAuthorized(true)
+            }else{
+                setIsAuthorized(false)
+            }
+        }catch(e){
+            setIsAuthorized(false)
+        }
 
 
 
     }
 
     const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if(!token){
+            setIsAuthorized(false)
+            return
+        }
+        const decoded = jwtDecode(token) // decodes token and gives us expiration date
+        const tokenExpiration = decoded.exp
+        const now = Date.now() / 1000
 
+        if(tokenExpiration > now){
+            await refreshToken()
+        }else{
+            setIsAuthorized(true)
+        }
 
 
 
