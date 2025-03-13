3f657d7d9fa243628574be57daa9d89fdca6d60f
donan928 <donan928@student.otago.ac.nz>
2025-03-13 17:06:18 +1300

    handle submit jsx

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -11,8 +11,27 @@ function Form({route, method}){
 
     const name = method === "login" ? "Login" : "Register"
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        setLoading(true)
         e.preventDefault() //prevent us from submitting form 
+
+        try{
+            const res = await api.post(route, {username, password})
+            if(method === "login"){
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+                navigate("/")
+            }else{
+                navigate("/login")
+            }
+
+
+        }catch(error){
+            alert(error)
+        }finally{
+            setLoading(false)
+        }
+
     }
 
     return <form onSubmit={handleSubmit} className="form-container">
