2f4fa6d37df88d40a7599b23ababcc3525387d1c
donan928 <donan928@student.otago.ac.nz>
2025-03-18 16:05:41 +1300

    views.py for exercise, sets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import UserSerializer, WorkoutSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Workout
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import UserSerializer, WorkoutSerializer, ExerciseSerializer, SetSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Workout, Exercise, Set
 
 class WorkoutListCreate(generics.ListCreateAPIView):
     serializer_class = WorkoutSerializer
@@ -20,6 +20,61 @@ class WorkoutListCreate(generics.ListCreateAPIView):
             serializer.save(owner=self.request.user)
         else:
             print(serializer.errors)
+            
+class WorkoutDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = WorkoutSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Workout.objects.filter(owner=user)
+            
+
+class ExerciseListCreate(generics.ListCreateAPIView):
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        workout_id = self.kwargs.get('workout_id')
+        workout = get_object_or_404(Workout, id=workout_id, owner=self.request.user)
+        return Exercise.objects.filter(workout=workout)
+    
+    def perform_create(self, serializer):
+        workout_id = self.kwargs.get('workout_id')
+        workout = get_object_or_404(Workout, id=workout_id, owner=self.request.user)
+        serializer.save(workout=workout)
+
+class ExerciseDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        workout_id = self.kwargs.get('workout_id')
+        workout = get_object_or_404(Workout, id=workout_id, owner=self.request.user)
+        return Exercise.objects.filter(workout=workout)
+
+class SetListCreate(generics.ListCreateAPIView):
+    serializer_class = SetSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        exercise_id = self.kwargs.get('exercise_id')
+        exercise = get_object_or_404(Exercise, id=exercise_id, workout__owner=self.request.user)
+        return Set.objects.filter(exercise=exercise)
+    
+    def perform_create(self, serializer):
+        exercise_id = self.kwargs.get('exercise_id')
+        exercise = get_object_or_404(Exercise, id=exercise_id, workout__owner=self.request.user)
+        serializer.save(exercise=exercise)
+
+class SetDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = SetSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        exercise_id = self.kwargs.get('exercise_id')
+        exercise = get_object_or_404(Exercise, id=exercise_id, workout__owner=self.request.user)
+        return Set.objects.filter(exercise=exercise)
             
             
 class WorkoutDelete(generics.DestroyAPIView):
